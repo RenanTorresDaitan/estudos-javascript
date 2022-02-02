19d62b132b300d8fd43bc4628189c00877a35734
Renan Torres <rtorres@daitan.com>
2022-02-02 13:49:45 -0300

    [EJS-04]: Solved first implementation of the every function

--- eloquent-javascript/EJS-04.js
+++ eloquent-javascript/EJS-04.js
@@ -11,12 +11,33 @@ console.log(`Flattened: ${flattenedArray}`);
 console.log("\n\tYour Own Loop\n");
 
 const loop = (value, test, update, body) => {
-    let iterations = value;
-    for (let i = 0; i < iterations; i++) {
+  let iterations = value;
+  for (let i = 0; i < iterations; i++) {
     if (!test) return false;
     body(value);
     value = update(value);
   }
 };
 console.log("loop(3, n => n > 0, n => n - 1, console.log)");
-loop(3, n => n > 0, n => n - 1, console.log);
+loop(
+  3,
+  (n) => n > 0,
+  (n) => n - 1,
+  console.log
+);
+
+console.log("\n\tEverything\n");
+
+const every = (array, test) => {
+  for (let i = 0; i < array.length; i++) {
+    if (!test(array[i])) return false;
+  }
+  return true;
+};
+
+console.log(every([1, 3, 5], (n) => n < 10));
+// → true
+console.log(every([2, 4, 16], (n) => n < 10));
+// → false
+console.log(every([], (n) => n < 10));
+// → true
