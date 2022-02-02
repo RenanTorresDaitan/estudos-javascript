d20389253320f49a39fff35090f0703a5702cb53
Renan Torres <rtorres@daitan.com>
2022-02-02 11:25:22 -0300

    [EJS-04]: Solved the Array Flattening exercise
    Fix: Logged the correct form of the array

--- eloquent-javascript/EJS-04.js
+++ eloquent-javascript/EJS-04.js
@@ -1,8 +1,20 @@
 console.log("Chapter 05 - Exercises\n");
-console.log("\t Array Flattening\n");
+console.log("\tArray Flattening\n");
 
 console.log("let arrays = [[1, 2, 3], [4, 5], [6]]");
 let arrays = [[1, 2, 3], [4, 5], [6]];
-let flattenedArray = []
-flattenedArray.push(arrays.reduce((result, element) => (result.concat(element))));
-console.log("Flattened: " + flattenedArray);
+const flattenedArray = arrays.reduce((result, element) => (result.concat(element)));
+console.log(`Flattened: ${flattenedArray}`);
+
+console.log("\n\tYour Own Loop\n");
+
+const loop = (value, test, update, body) => {
+    let iterations = value;
+    for (let i = 0; i < iterations; i++) {
+    if (!test) return false;
+    body(value);
+    value = update(value);
+  }
+};
+console.log("loop(3, n => n > 0, n => n - 1, console.log)");
+loop(3, n => n > 0, n => n - 1, console.log);
