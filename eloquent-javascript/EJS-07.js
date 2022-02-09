7b929eeabc3715fa765c5be053348a3f6b488b09
Renan Torres <rtorres@daitan.com>
2022-02-09 13:42:59 -0300

    [EJS-07]: Solved the Locked Box  exercise

--- eloquent-javascript/EJS-07.js
+++ eloquent-javascript/EJS-07.js
@@ -34,3 +34,58 @@ function reliableMultiply(a, b) {
 console.log(reliableMultiply(8, 8));
 // → 64
 
+console.log("\tThe Locked Box\n");
+
+/*It is a box with a lock. There is an array in the box, but you can 
+get at it only when the box is unlocked. Directly accessing the 
+private _content property is forbidden.
+
+Write a function called withBoxUnlocked that takes a function 
+value as argument, unlocks the box, runs the function, and then 
+ensures that the box is locked again before returning, regardless 
+of whether the argument function returned normally or threw an exception.
+*/
+const box = {
+  locked: true,
+  unlock() {
+    this.locked = false;
+  },
+  lock() {
+    this.locked = true;
+  },
+  _content: [],
+  get content() {
+    if (this.locked) throw new Error("Locked!");
+    return this._content;
+  },
+};
+
+function withBoxUnlocked(body) {
+  if (box.locked) {
+    box.unlock();
+  }
+  try {
+    body();
+  } catch (error) {
+    throw error;
+  } finally {
+    box.lock();
+  }
+}
+
+withBoxUnlocked(function () {
+  box.content.push("gold piece");
+});
+
+try {
+  withBoxUnlocked(function () {
+    throw new Error("Pirates on the horizon! Abort!");
+  });
+} catch (e) {
+  console.log("Error raised: " + e);
+}
+console.log(box.locked);
+// → true
+
+/*For extra points, make sure that if you call withBoxUnlocked 
+when the box is already unlocked, the box stays unlocked.*/
