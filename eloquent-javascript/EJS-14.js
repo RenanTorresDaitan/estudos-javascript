5aacc7108075bbff0efdaf8823ff9f0a2a3dc7b6
Renan Torres <rtorres@daitan.com>
2022-03-02 13:06:34 -0300

    [EJS-14]: Solved the 'Pause the game' exercise, first part

--- eloquent-javascript/EJS-14.js
+++ eloquent-javascript/EJS-14.js
@@ -433,3 +433,52 @@ async function runGame(plans, Display) {
 }
 runGame(GAME_LEVELS, DOMDisplay);
 
+let pauseTheGameSubheading = document.createElement("h2");
+pauseTheGameSubheading.textContent = "Pausing the Game";
+document.body.appendChild(pauseTheGameSubheading);
+
+/*Make it possible to pause (suspend) and unpause the game by pressing the Esc key.
+
+This can be done by changing the runLevel function to use another keyboard 
+event handler and interrupting or resuming the animation whenever the Esc 
+key is hit.
+
+The runAnimation interface may not look like it is suitable for this at 
+first glance, but it is if you rearrange the way runLevel calls it.
+
+When you have that working, there is something else you could try. 
+The way we have been registering keyboard event handlers is somewhat problematic. 
+The arrowKeys object is currently a global binding, and its event handlers are 
+kept around even when no game is running. You could say they leak out of our system. 
+Extend trackKeys to provide a way to unregister its handlers and then change 
+runLevel to register its handlers when it starts and unregister them again 
+when it is finished.*/
+
+function runLevel(level, Display) {
+  let display = new Display(document.body, level);
+  let state = State.start(level);
+  let ending = 1;
+  let paused = false;
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "Escape") paused = !paused;
+  });
+  return new Promise((resolve) => {
+    runAnimation((time) => {
+      if (!paused) {
+        state = state.update(time, arrowKeys);
+      }
+      display.syncState(state);
+      if (state.status == "playing") {
+        return true;
+      } else if (ending > 0) {
+        ending -= time;
+        return true;
+      } else {
+        display.clear();
+        resolve(state.status);
+        return false;
+      }
+    });
+  });
+}
+runGame(GAME_LEVELS, DOMDisplay);
