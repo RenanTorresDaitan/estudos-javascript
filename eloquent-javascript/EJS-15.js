97f98ad2b4935af6ee06125de375fe12746bc781
Renan Torres <rtorres@daitan.com>
2022-03-04 08:39:51 -0300

    [EJS-15]: Drawed the second shape 'Diamond' from the exercise

--- eloquent-javascript/EJS-15.js
+++ eloquent-javascript/EJS-15.js
@@ -1,5 +1,7 @@
 let cx = document.querySelector("canvas").getContext("2d");
 
+const { height, width } = cx.canvas;
+
 function trapezoid(xPos, yPos, size, largerSide = size / 3) {
   const top =
     largerSide > 0 ? (largerSide > size ? largerSide : largerSide) : size;
@@ -15,4 +17,14 @@ function trapezoid(xPos, yPos, size, largerSide = size / 3) {
   cx.stroke();
 }
 
-trapezoid(50, 75, 25, -45);
+trapezoid(50, height / 2, 25, -45);
+
+function diamond(xPos, yPos, size, color) {
+  cx.translate(xPos, yPos);
+  cx.fillStyle = color;
+  cx.rotate(Math.PI / 4);
+  cx.fillRect(-(size / 2), -(size / 2), size, size);
+  cx.resetTransform();
+}
+
+diamond(150, height / 2, 50, "red");
