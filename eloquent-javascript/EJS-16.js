15be2b5b5dd51fea47c2a7ecb2a39e384287f18a
Renan Torres <rtorres@daitan.com>
2022-03-09 10:30:04 -0300

    [EJS-16]: Solved the 'A JavaScript WorkBench' exercise

--- eloquent-javascript/EJS-16.js
+++ eloquent-javascript/EJS-16.js
@@ -1,3 +1,5 @@
+// Content Negotiation exercise
+
 const fetchDiv = document.querySelector("#fetchResult");
 
 fetchDiv.textContent = "Fetching result...";
@@ -10,9 +12,27 @@ const acceptHeaders = [
 ];
 
 acceptHeaders.map((header) => {
-    fetch("https://eloquentjavascript.net/author", {
-  headers: { accept: header },
-})
-  .then((resp) => {return `<div>Header: ${header} Code: ${resp.status} Text: ${resp.statusText}</div>`})
-  .then((text) => fetchDiv.innerHTML += text);
+  fetch("https://eloquentjavascript.net/author", {
+    headers: { accept: header },
+  })
+    .then((resp) => {
+      return `<div>Header: ${header} Code: ${resp.status} Text: ${resp.statusText}</div>`;
+    })
+    .then((text) => (fetchDiv.innerHTML += text));
+});
+
+// A JavaScript WorkBench Exercise
+
+const codeTextArea = document.querySelector("#code");
+const buttonRunCode = document.querySelector("#button");
+let codeOutput = document.querySelector("#output");
+
+buttonRunCode.addEventListener("click", () => {
+  let code = "";
+  try {
+    code = Function(codeTextArea.value)();
+  } catch (err) {
+    code = err;
+  }
+  codeOutput.appendChild(document.createTextNode(`${code}\n`));
 });
