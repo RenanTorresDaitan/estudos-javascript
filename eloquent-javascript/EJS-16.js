9f14b54c7bedc3123cb50a58ce1b23bc43e62b87
Renan Torres <rtorres@daitan.com>
2022-03-10 10:45:26 -0300

    [EJS-16]: Conway's Game of Life first working prototype, needs optimization

--- eloquent-javascript/EJS-16.js
+++ eloquent-javascript/EJS-16.js
@@ -41,6 +41,7 @@ buttonRunCode.addEventListener("click", () => {
 
 const conwayGrid = document.querySelector("#grid");
 const nextButton = document.querySelector("#next");
+const autoRunButton = document.querySelector("#autoRun");
 
 class Matrix {
   constructor(width, height, element = (x, y) => undefined) {
@@ -62,59 +63,94 @@ class Matrix {
     this.content[y * this.width + x] = value;
   }
 }
-const MATRIX_WIDTH = 5;
-const MATRIX_HEIGHT = 5;
+const MATRIX_WIDTH = 15;
+const MATRIX_HEIGHT = 15;
 
-const gridTest = new Matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
+const initialGrid = new Matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
 for (let i = 0; i < MATRIX_HEIGHT; i++) {
   for (let j = 0; j < MATRIX_WIDTH; j++) {
-    const checkbox = document.createElement("input");
-    checkbox.type = "checkbox";
-    checkbox.checked = Math.random() < 0.25 ? true : false;
-    gridTest.set(j, i, checkbox);
-    grid.appendChild(gridTest.get(j, i));
+    let checkbox = Math.random() < 0.25 ? true : false;
+    initialGrid.set(j, i, checkbox);
   }
-  grid.appendChild(document.createElement("br"));
 }
 
-nextButton.addEventListener("click", nextGrid.bind(this, gridTest));
-function nextGrid(grid) {
-  const newGrid = new Matrix(grid.width, grid.height);
-
-  for (let i = 0; i < newGrid.height; i++) {
-    for (let j = 0; j < newGrid.width; j++) {
+function createCheckboxFromGrid(grid) {
+  for (let i = 0; i < MATRIX_HEIGHT; i++) {
+    for (let j = 0; j < MATRIX_WIDTH; j++) {
       const checkbox = document.createElement("input");
       checkbox.type = "checkbox";
-      checkbox.checked = false;
-      newGrid.set(j, i, checkbox);
+      checkbox.checked = grid.get(j, i);
+      conwayGrid.appendChild(checkbox);
+    }
+    conwayGrid.appendChild(document.createElement("br"));
+  }
+}
+
+function createGridFromCheckboxes(nodeElement) {
+  let checkboxState = Array.from(nodeElement.querySelectorAll("input")).map(
+    (item) => item.checked
+  );
+  const grid = new Matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
+  for (let i = 0; i < MATRIX_HEIGHT; i++) {
+    for (let j = 0; j < MATRIX_WIDTH; j++) {
+      let checkbox = checkboxState[j + i * MATRIX_WIDTH];
+      grid.set(j, i, checkbox);
     }
   }
-  console.log(newGrid);
-  console.log(grid);
-  for (let y = 0; y < grid.height; y++) {
-    for (let x = 0; x < grid.width; x++) {
-      let neighborCells = 0;
-      try {
-        neighborCells += grid.get(x - 1, y - 1).checked ? 1 : 0;
-        neighborCells += grid.get(x - 1, y).checked ? 1 : 0;
-        neighborCells += grid.get(x - 1, y + 1).checked ? 1 : 0;
-        neighborCells += grid.get(x, y - 1).checked ? 1 : 0;
-        neighborCells += grid.get(x, y + 1).checked ? 1 : 0;
-        neighborCells += grid.get(x + 1, y - 1).checked ? 1 : 0;
-        neighborCells += grid.get(x + 1, y).checked ? 1 : 0;
-        neighborCells += grid.get(x + 1, y + 1).checked ? 1 : 0;
-      } catch {
-      } finally {
-        if (!grid.get(x, y).checked && neighborCells === 3)
-          newGrid.set(x, y).checked = true;
-        if (
-          grid.get(x, y).checked &&
-          (neighborCells === 2 || neighborCells === 3)
-        )
-          newGrid.set(x, y).checked = true;
-        if (grid.get(x, y).checked && (neighborCells < 2 || neighborCells > 3))
-          newGrid.set(x, y).checked = false;
+  return grid;
+}
+
+createCheckboxFromGrid(initialGrid);
+
+function generateNextGrid(previousGrid) {
+  const nextGrid = new Matrix(MATRIX_WIDTH, MATRIX_HEIGHT);
+  for (let y = 0; y < MATRIX_HEIGHT; y++) {
+    for (let x = 0; x < MATRIX_WIDTH; x++) {
+      let neighborCells = countNeighborCells(previousGrid, x, y);
+      if (!previousGrid.get(x, y) && neighborCells === 3) nextGrid.set(x, y, true);
+      if (previousGrid.get(x, y) && (neighborCells === 2 || neighborCells === 3))
+        nextGrid.set(x, y, true);
+      if (previousGrid.get(x, y) && (neighborCells < 2 || neighborCells > 3))
+        nextGrid.set(x, y, false);
+    }
+  }
+  while (conwayGrid.firstChild) {
+    conwayGrid.removeChild(conwayGrid.firstChild);
+  }
+  createCheckboxFromGrid(nextGrid);
+}
+
+function countNeighborCells(grid, x, y) {
+  let neighborCells = 0;
+  for (
+    let y1 = Math.max(0, y - 1);
+    y1 <= Math.min(MATRIX_HEIGHT - 1, y + 1);
+    y1++
+  ) {
+    for (
+      let x1 = Math.max(0, x - 1);
+      x1 <= Math.min(MATRIX_WIDTH - 1, x + 1);
+      x1++
+    ) {
+      if ((x1 != x || y1 != y) && grid.get(x1, y1)) {
+        neighborCells++;
       }
     }
   }
+  return neighborCells;
 }
+
+function run() {
+  generateNextGrid(createGridFromCheckboxes(conwayGrid));
+}
+nextButton.addEventListener("click", () => run());
+
+let running = null;
+autoRunButton.addEventListener("click", () => {
+  if (running) {
+    clearInterval(running);
+    running = null;
+  } else {
+    running = setInterval(run, 500);
+  }
+});
